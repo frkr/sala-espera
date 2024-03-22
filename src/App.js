cbc0ead6ac832c4d371ef88b14f11320137e71b3
DaviSM <davimesquita@gmail.com>
2024-03-22 18:22:40 -0300

    feat: Exemplo de pagina

--- src/App.js
+++ src/App.js
@@ -1,56 +1,68 @@
 import './index.css'
-import { useState, useEffect } from 'react'
-import { createClient } from '@supabase/supabase-js'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { jwtDecode } from "jwt-decode";
-
+import React, {Fragment, useEffect, useState} from 'react'
+import {createClient} from '@supabase/supabase-js'
 
 const supabase = createClient(
     'https://bwlxwwtjuqdiuloefuux.supabase.co',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3bHh3d3RqdXFkaXVsb2VmdXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAyOTE3NzksImV4cCI6MjAyNTg2Nzc3OX0.g-3wuPVmiZDe1Gr3SbZxg7eKKUEfmCV0ii196Vw0UIU'
 )
 
-export  function App() {
-    const [session, setSession] = useState(null)
-    const [token, setToken] = useState('')
+export function App() {
+    // const [session, setSession] = useState(null)
+    const [token, setToken] = useState(null)
 
     useEffect(() => {
-        try {
-            const decodedHeader = jwtDecode(token, {header: true});
-            console.log(decodedHeader);
-
-            setToken(JSON.stringify(decodedHeader, null, 2));
-        } catch (e) {
-            console.log(e);
-        }
-    }, [session])
-
-    useEffect(() => {
-        supabase.auth.getSession().then(({ data: { session } }) => {
-            setSession(session)
-        })
-
-        const {
-            data: { subscription },
-        } = supabase.auth.onAuthStateChange((_event, session) => {
-            setSession(session)
-        })
-
+        const {data: {user}} = supabase.auth.getUser().then(d => d);
+        setToken(JSON.stringify(user, null, 2));
+        // supabase.auth.getSession().then(({data: {session}}) => {
+        //     setSession(session)
+        // })
+        //
+        // const {
+        //     data: {subscription},
+        // } = supabase.auth.onAuthStateChange((_event, session) => {
+        //     if (_event === "SIGNED_IN") {
+        //         const {data: {user}} = supabase.auth.getUser().then(d => d);
+        //         setToken(JSON.stringify(user, null, 2));
+        //     } else {
+        //         setTimeout(() => {
+        //             supabase.auth.signInWithOAuth({
+        //                 provider: 'google',
+        //                 options: {
+        //                     queryParams: {
+        //                         access_type: 'offline',
+        //                         prompt: 'consent',
+        //                     },
+        //                 },
+        //             })
+        //         }, 1000)
+        //     }
+        // })
+        //
         return () => subscription.unsubscribe()
     }, [])
 
-    if (!session) {
-        return <div><Auth
-            supabaseClient={supabase}
-            appearance={{ theme: ThemeSupa }}
-            providers={['google']}
-        />
-        </div>
-    }
-    else {
-        return <div>Logged in! {token}</div>
-    }
+    return (
+        <main>
+            <div className='mx-5 py-4'>
+
+                <div className='p-4 row align-items-center rounded-3 border shadow-lg'>
+
+                    <h1 id="countdown"></h1>
+                    <p className="sub-title">
+                        {!session ? '' :
+                            <Fragment>
+                                Logado como:
+                                {token}
+                            </Fragment>
+                        }
+                    </p>
+
+                </div>
+            </div>
+        </main>
+    )
 }
+
 
 export default App;
